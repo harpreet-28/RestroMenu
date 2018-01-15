1f002a64f36a60d0d5906f72c59557236f5fc1c3
preet.k.k88@gmail.com <preetkaur@Preets-MBP.home>
2018-01-15 13:05:55 -0500

    feedback updation :poop:

--- js/app.js
+++ js/app.js
@@ -1,10 +1,34 @@
 
+// (function(){
+
+
+
+
+// // const logo = document.getElementsByClassName('logoDescription');
+// // console.log(logo);
+
+// // function changeColor(){
+// // 	logo[0].style.color = 'yellow';
+// // }
+// // logo[0].addEventListener('click', changeColor, false);
+
+
+// const openMenu = $('.openMenu')
+
+// console.log(openMenu)
+
+// $(openMenu).on('click', function() {
+// 				var position = $('input[name="position"]:checked').val();
+// 				$('.pure-drawer').attr('data-position', position)});
+// })();
+// 
 $(document).ready(function($) {
 	$('#submitFeedback').click(function(){
+		let customerEmail = $('#customerEmail').val();
 		let feedbackListItem = $("#customerFeedback").val();
-		let customerName = $('#customerName').val().toUpperCase();
+		let customerName = $('#customerName').val().toUpperCase().bold();
 		if(feedbackListItem.length > 0){
-			$("#displayFeedbackList").append('<li>'+ feedbackListItem  + '	-'+ customerName +'</li>')
+			$("#displayFeedbackList").append('<li>'+ feedbackListItem  + '	--'+ customerName+ ' ('+ customerEmail+ ')' +'</li>')
 			$("#customerFeedback").val("");
 		};
 			});
